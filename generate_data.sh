fae88a6fc2813a97294e8805e6b0830eecdd8b53
manuelmai <manuel.mai@yale.edu>
2014-06-20 15:40:03 -0400

    added a sleep to simple_for_loop,sh

--- generate_data.sh
+++ generate_data.sh
@@ -4,7 +4,9 @@
 
 for i in {1..10};
 do
-let j=1+i 
+let j=i+1 
 echo $i $j >> data1.txt;
 done
-
+awk '{print $1,$2^2}' data1.txt > data1_new.txt
+cat data1_new.txt > data1.txt
+rm data1_new.txt
--- simple_for_loop.sh
+++ simple_for_loop.sh
@@ -4,4 +4,4 @@ for i in {1..10};
 do 
 echo "j=" $i;
 done
-
+sleep 10000
